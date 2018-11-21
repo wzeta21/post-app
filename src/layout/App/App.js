07a0daa86e613e0ae2e8902381e36a35500dae44
Wilder Zapata Miranda <wzeta21@gmail.com>
2018-11-21 13:38:52 -0400

    my firt stateless componet

--- src/layout/App/App.js
+++ src/layout/App/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import logo from './../../logo.svg';
 import './App.css';
+import { DataTable } from '../../components/index';
 
+const templateFuncion = (a) => a;
+
+const columns = [
+  {'field': 'id', 'label': 'Code', 'template': templateFuncion},
+  {'field': 'name', 'label': 'Name', 'template': templateFuncion},
+  {'field': 'lastname', 'label': 'Last name', 'template': templateFuncion},
+  {'field': 'nick', 'label': 'Nick name', 'template': templateFuncion},
+];
+
+const rows = [
+  {
+  'id': 100,
+  'name':'Luis Carlos',
+  'lastname': 'Prestes',
+  'nick': 'carlinho',
+  '1': 'cell 1'
+},
+{
+  'id': 200,
+  'name':'Nossa senhora',
+  'lastname': 'da Aprecida',
+  'nick': 'Cida',
+  '1': 'cell 1'
+}
+];
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+                  
             Learn React
-          </a>
+            <DataTable columns ={columns} rows={rows}/>
         </header>
       </div>
     );
