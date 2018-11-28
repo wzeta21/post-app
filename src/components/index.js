73380f9d2da9804a68a202f6450fb84dfbb5cec7
Wilder Zapata Miranda <wzeta21@gmail.com>
2018-11-28 12:05:05 -0400

    delete some files

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,7 @@
-import DataTable from './DataTable';
-import AddUser from './AddUser';
 import PostsDDL from './PostsDDL';
 import UserForm from './UserForm';
 import PostManager from './PostManager';
 import PostCard from './PostCard';
 import MenuApp from './MenuApp';
 
-export { DataTable, AddUser, PostsDDL, UserForm, PostManager, PostCard, MenuApp };
+export { PostsDDL, UserForm, PostManager, PostCard, MenuApp };
